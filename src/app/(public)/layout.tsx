032728af05c0c98f6b0806e13c8f51991662d54f
DenisEvlampiev23 <dzianis.yaulampiyeu@icloud.com>
2026-02-03 15:47:15 +0300

    feat: theme control

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "../globals.css";
+import { ThemeProvider } from "next-themes";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -8,9 +9,16 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        {children}
+        <ThemeProvider 
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/forms/signUpForm.tsx
+++ src/components/forms/signUpForm.tsx
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge";
 
 function SignUpForm({ className }: { className?: string }) {
     return (
-        <form className={twMerge("bg-red-500 w-12 h-12", className)}>
+        <form className={twMerge("bg-card w-12 h-12", className)}>
 
         </form>
     );
